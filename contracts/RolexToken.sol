4b258455d3d6b7712f8c973eb348366c29d0310b
Ivan Solomichev <vanijatko.social@gmail.com>
2018-02-11 23:14:18 +0200

    Initialization with OpenZeppelin potential bug

---
+++ contracts/RolexToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.18;
+
+import "../node_modules/zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+
+contract RolexToken is MintableToken {
+
+  string public constant name = "Rolex Token";
+  string public constant symbol = "RXT";
+  uint8 public constant decimals = 1;  // 18 is the most common number of decimal places
+
+  function RolexToken() public {
+    totalSupply_ = 1; //  it works inly with positive value. Is I remove this line, I get error, while trying to mint coins.
+  }
+}
---
+++ migrations/2_ico_migration.js
@@ -0,0 +1,16 @@
+var RolexICO = artifacts.require("./RolexICO.sol");
+
+
+module.exports = function (deployer) {
+    //  function RolexICO(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, uint256 _icoCap, uint _tokenTotalSupplyLimit, uint8 _icoTokenPercents)
+    const _startTime = 1518382496;
+    const _endTime = 1518391500;
+    const _rate = 1; //  token per wei
+    const _wallet = web3.eth.accounts[4];
+    const _icoCap = 2000000000000000000; //  2 ETH
+    const decimals = 1;
+    const _tokenTotalSupplyLimit = 1000000; // 50 * 10 ** decimals; //  50 tokens
+    const _icoTokenPercents = 65; //  %
+
+    deployer.deploy(RolexICO, _startTime, _endTime, _rate, _wallet, _icoCap);
+};
